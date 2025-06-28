58575f8ecc705ef1439f1438cc8008c09ff1c6b5
carriegale2710 <carriegale2710@gmail.com>
2025-06-28 17:39:28 +1000

    simplifes  filtering by category: replaces toggle button filter with simple select dropwdown on TaskPage

--- front-end/src/components/Category/CategoryTag/CategoryTag.tsx
+++ front-end/src/components/Category/CategoryTag/CategoryTag.tsx
@@ -1,31 +1,13 @@
-import { useContext } from "react";
 import type { Category } from "../../../services/categories";
 import Button from "../../Button/Button";
 import classes from "./CategoryTag.module.scss";
-import { CategoryFilterContext } from "../../../context/CategoryFilterContextProvider";
 
 interface CategoryTagProps {
   category: Category;
 }
 
 const CategoryTag = ({ category }: CategoryTagProps) => {
-  const { categoryFilter, setCategoryFilter } = useContext(
-    CategoryFilterContext
-  );
-
-  const taskFilter = () => {
-    categoryFilter.some((c) => c.id === category.id)
-      ? setCategoryFilter(categoryFilter.filter((c) => c.id !== category.id)) //turn filter off
-      : setCategoryFilter([...categoryFilter, category]); //turn filter on
-  };
-
-  return (
-    category && (
-      <Button className={classes.btn} onClick={taskFilter}>
-        {category?.name}
-      </Button>
-    )
-  );
+  return category && <Button className={classes.btn}>{category?.name}</Button>;
 };
 
 export default CategoryTag;
