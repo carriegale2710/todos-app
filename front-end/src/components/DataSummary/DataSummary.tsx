70aeff7a59bbbde00fba228ef0577aa4b49852d8
carriegale2710 <carriegale2710@gmail.com>
2025-07-02 02:24:48 +1000

    creates DataSummary to separate out logic from TasksPage

---
+++ front-end/src/components/DataSummary/DataSummary.tsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { TaskListContext } from "../../context/TaskListContextProvider";
+import { CategoryListContext } from "../../context/CategoryListContextProvider";
+import classes from "./DataSummary.module.scss";
+
+const DataSummary = () => {
+  const { taskList } = useContext(TaskListContext);
+  const { categoryList } = useContext(CategoryListContext);
+  return (
+    <section className={classes.row}>
+      <p>Tasks: {taskList.length}</p>
+      <p>Categories: {categoryList.length}</p>
+    </section>
+  );
+};
+
+export default DataSummary;
