292a528cd6fa36ed485fa986a92ce1104e4b6d5b
carriegale2710 <carriegale2710@gmail.com>
2025-07-04 02:52:12 +1000

    Icon display component

---
+++ front-end/src/components/presentational/Icon/Icon.tsx
@@ -0,0 +1,15 @@
+const Icon = ({ path }: { path: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      width="24"
+      height="24"
+    >
+      <path d={path} />
+    </svg>
+  );
+};
+
+export default Icon;
--- front-end/src/components/presentational/Modal/Modal.tsx
+++ front-end/src/components/presentational/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import Button from "../Button/Button";
 import Header from "../Header/Header";
+import Icon from "../Icon/Icon";
 import classes from "./Modal.module.scss";
 
 interface ModalProps {
@@ -18,15 +19,7 @@ const Modal: React.FC<ModalProps> = ({ heading, children, onClose }) => {
           onClick={onClose}
           variants={["icon-btn", "hover_highlight"]}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            width="24"
-            height="24"
-          >
-            <path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
-          </svg>
+          <Icon path="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
         </Button>
       </header>
       {children}
